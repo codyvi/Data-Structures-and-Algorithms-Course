4be435a09096d9d6bc6787c89d433177db93e2c7
codyvi <davidcantumartinez@gmail.com>
2019-12-26 22:56:37 -0600

    Done with quicksort

--- Week 4 Hw/QuickSortImprove.cpp
+++ Week 4 Hw/QuickSortImprove.cpp
@@ -18,6 +18,34 @@ int partition2(vector<int> &a, int l, int r) {
   return j;
 }
 
+void partition3(vector<int> &a, int l, int r, int tmp[])
+{
+    int x = a[l];
+    tmp[0] = l;
+    tmp[1] = l;
+
+    for(int i = l+1; i <= r; i++)
+    {
+        if(a[i] < x)
+        {
+            tmp[0]++;
+            tmp[1]++;
+            swap(a[tmp[0]], a[tmp[1]]);
+            if(tmp[1] != i)
+            {
+                swap(a[tmp[0]], a[i]);
+            }
+        }
+        else if (a[i] == x)
+        { 
+            tmp[1]++;
+            swap(a[tmp[1]], a[i]);
+        }
+    }
+
+    swap(a[l], a[tmp[0]]);
+}
+
 void randomized_quick_sort(vector<int> &a, int l, int r) {
   if (l >= r) {
     return;
@@ -25,10 +53,10 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
-
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  int m[2] = { 0 };
+  partition3(a,l,r,m);
+  randomized_quick_sort(a, l, m[0] - 1);
+  randomized_quick_sort(a, m[1] + 1, r);
 }
 
 int main() {
