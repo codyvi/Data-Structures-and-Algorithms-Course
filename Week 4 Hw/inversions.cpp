aa4abd2a8e5f91c8ba4230622545b8e84ef46275
codyvi <davidcantumartinez@gmail.com>
2019-12-28 20:00:06 -0600

    Inversions done

---
+++ Week 4 Hw/inversions.cpp
@@ -0,0 +1,80 @@
+
+#include <bits/stdc++.h> 
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+long long _mergeSort(vector<int> &a, int temp[], int left, int right); 
+long long merge(vector<int> &a, int temp[], int left, int mid, int right); 
+
+
+long long mergeSort(vector <int> &a, int array_size) 
+{ 
+	int temp[array_size]; 
+	return _mergeSort(a, temp, 0, array_size - 1); 
+} 
+
+long long _mergeSort(vector<int> &a, int temp[], int left, int right) 
+{ 
+	int mid, inv_count = 0; 
+	if (right > left) { 
+
+		mid = (right + left) / 2; 
+
+
+		inv_count += _mergeSort(a, temp, left, mid); 
+		inv_count += _mergeSort(a, temp, mid + 1, right); 
+
+		inv_count += merge(a, temp, left, mid + 1, right); 
+	} 
+	return inv_count; 
+} 
+
+
+long long merge(vector<int> &a, int temp[], int left, 
+		int mid, int right) 
+{ 
+	int i, j, k; 
+	int inv_count = 0; 
+
+	i = left; 
+	j = mid; 
+	k = left; 
+	while ((i <= mid - 1) && (j <= right)) { 
+		if (a[i] <= a[j]) { 
+			temp[k++] = a[i++]; 
+		} 
+		else { 
+			temp[k++] = a[j++]; 
+
+
+			inv_count = inv_count + (mid - i); 
+		} 
+	} 
+
+	while (i <= mid - 1) 
+		temp[k++] = a[i++]; 
+
+	while (j <= right) 
+		temp[k++] = a[j++]; 
+
+	for (i = left; i <= right; i++) 
+		a[i] = temp[i]; 
+
+	return inv_count; 
+} 
+
+
+int main() 
+{ 
+    int n;
+    std::cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); i++) {
+        std::cin >> a[i];
+    }
+	
+	cout << mergeSort(a,n) << endl;
+	return 0; 
+} 
